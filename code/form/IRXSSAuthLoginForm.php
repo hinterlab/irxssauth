fd2d68f17bba520414d9486b8d7c0370ca58ae99
Yuchen Liu <yuchen.liu@internetrix.com.au>
2016-11-25 09:48:02 +1100

    Update IRXSSAuthLoginForm.php add extended function to excute before MemberLoginForm construction

--- code/form/IRXSSAuthLoginForm.php
+++ code/form/IRXSSAuthLoginForm.php
@@ -3,4 +3,12 @@ class IRXSSAuthLoginForm extends MemberLoginForm {
 	
 	protected $authenticator_class = 'IRXSSAuthenticator';
 	
+	public function __construct($controller, $name, $fields = null, $actions = null,
+			$checkCurrentUser = true) {
+		
+		$this->extend('preConstruct', $controller, $name, $fields, $actions, $checkCurrentUser);
+	
+		parent::__construct($controller, $name, $fields, $actions);
+	}
+	
 }
