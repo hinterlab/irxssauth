a807b0e301f3e0e5198a149a009525bda4611920
Yuchen Liu <yuchen.liu@internetrix.com.au>
2017-01-20 19:29:31 +1100

    Update IRXAuthControllerExtension.php

--- code/extensions/IRXAuthControllerExtension.php
+++ code/extensions/IRXAuthControllerExtension.php
@@ -12,9 +12,26 @@ class IRXAuthControllerExtension extends DataExtension {
 	}
 	
 	public function protect_site_from_indexing() {
-		$config = Config::inst()->forClass('IRXBasicAuth');
-		if($config->entire_site_protected) {
-			self::requireLogin($config->entire_site_protected_message, $config->entire_site_protected_code, false);
+		if($this->owner->basicAuthEnabled){
+			if( defined('SS_USE_BASIC_AUTH') && SS_USE_BASIC_AUTH ){
+				return true;
+			}else{
+				
+				$config = Config::inst()->forClass('IRXBasicAuth');
+				$stagingDomains = $config->StagingDomainFeaturedStrings;
+				$isStaging = false;
+				
+				foreach($stagingDomains as $domain){
+					if( strpos( $_SERVER['HTTP_HOST'], $domain ) !== false) $isStaging = true;
+				}
+				
+				if($config->staging_site_protected && $isStaging) {
+					return true;
+				}
+				
+			}
 		}
+		
+		return false;
 	}
 }
