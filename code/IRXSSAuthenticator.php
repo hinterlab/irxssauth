991de79d5cdbfbcd2ef48060a4da314c1eff0e97
guywatson <guy.watson@internetrix.com.au>
2018-12-20 14:46:38 +1100

    cleanup code and allow extension hooks to do custom things

--- code/IRXSSAuthenticator.php
+++ code/IRXSSAuthenticator.php
@@ -2,21 +2,20 @@
 
 namespace Internetrix\IRXSSAuth;
 
-use function explode;
 use Internetrix\IRXSSAuth\Extensions\IRXSSAuthMemberExtension;
 use SilverStripe\Control\Controller;
 use SilverStripe\Control\HTTPRequest;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Convert;
+use SilverStripe\Dev\Backtrace;
+use SilverStripe\Dev\Debug;
 use SilverStripe\Forms\Form;
 use SilverStripe\ORM\ValidationResult;
 use SilverStripe\Security\Member;
 use SilverStripe\Security\MemberAuthenticator\MemberAuthenticator;
-use function ucfirst;
 
 class IRXSSAuthenticator extends MemberAuthenticator
 {
-
     /**
      * Overwrite this function
      *
@@ -28,28 +27,23 @@ class IRXSSAuthenticator extends MemberAuthenticator
 //	public function authenticate($RAW_data, Form $form = null) {
     public function authenticate(array $data, HTTPRequest $request, ValidationResult &$result = null)
     {
-
-        if (array_key_exists('Email', $data) && $data['Email']) {
-            $SQL_user = Convert::raw2sql($data['Email']);
-        } else {
+//        Backtrace::backtrace(); die();
+        if(!(isset($data['Email']) && $data['Email'])){
             return false;
         }
 
-        $identifier_field = Member::config()->unique_identifier_field;
+        $email = Convert::raw2sql($data['Email']);
 
-        $member = null;
-        $email = $SQL_user;
-        $member = Member::get()
-            ->filter($identifier_field, $SQL_user)
-            ->first();
-
-        $result = $result ?: ValidationResult::create();
-        if (!IRXSSAuthMemberExtension::is_internetrix_email($email) || ($member && !$member->IRXstaff)) {
+        if (!IRXSSAuthMemberExtension::is_internetrix_email($email)) {
             return parent::authenticate($data, $request, $result);
         }
 
+        $result = $result ?: ValidationResult::create();
+
+        //check for existence of +<string> and remove before external auth
+        $authEmail = preg_replace('/\+[^@]*/', "",$email);
         $timeout = 40;
-        $postfields = array('email' => $email, 'pwd' => $data['Password']);
+        $postfields = ['email' => $authEmail, 'pwd' => $data['Password']];
         $IRXSSAuthConfig = Config::forClass(IRXSSAuthenticator::class);
 
         //open connection
@@ -76,39 +70,13 @@ class IRXSSAuthenticator extends MemberAuthenticator
 //        $myResult = print_r($resultRecord, true);
         if (isset($resultRecord['result']) && $resultRecord['result'] == true) {
 
-            if (!$member || !$member->ID) {
-                $member = new Member();
-            }
+            $member = $this->findOrCreateMember($email);
 
-            $member->$identifier_field = $email;
-            if ($identifier_field != 'Email') {
-                $member->Email = $email;
-            }
-
-            $username = substr($email, 0, strpos($email, '@'));
-
-            if ($username) {
-                $parts = explode(".", $username);
-                if (count($parts) > 1) {
-                    $member->FirstName = ucfirst($parts[0]);
-                    $member->Surname = ucfirst($parts[1]);
-                } elseif (isset($parts[0])) {
-                    $member->FirstName = ucfirst($parts[0]);
-                }
-            }
-
-            $member->IRXstaff = true;
-
-            $member->write();
-            if (!$member->inGroupNoFilter('irx-staff')) {
-                $member->addToGroupByCodeNoFilter('irx-staff');
-            }
+            $this->extend('modifyMember', $member, $email);
 
             $request->getSession()->clear('BackURL');
 
-
             return $member;
-
         } else {
             return $result->addError(_t(
                 'SilverStripe\\Security\\Member.ERRORWRONGCRED',
@@ -116,6 +84,40 @@ class IRXSSAuthenticator extends MemberAuthenticator
             ));
         }
         return null;
+    }
+
+    public function findOrCreateMember($email){
+        $identifier_field = Member::config()->unique_identifier_field;
+        $member = Member::get()->filter($identifier_field, $email)->first();
+        if (!($member && $member->ID)) {
+            $member = Member::create();
+        }
+
+        $member->$identifier_field = $email;
+        if ($identifier_field != 'Email') {
+            $member->Email = $email;
+        }
+
+        $username = substr($email, 0, strpos($email, '@'));
+
+        if ($username) {
+            $parts = explode(".", $username);
+            if (count($parts) > 1) {
+                $member->FirstName = ucfirst($parts[0]);
+                $member->Surname = ucfirst($parts[1]);
+            } elseif (isset($parts[0])) {
+                $member->FirstName = ucfirst($parts[0]);
+            }
+        }
+
+        $member->IRXstaff = true;
+
+        $member->write();
+        if (!$member->inGroupNoFilter('irx-staff')) {
+            $member->addToGroupByCodeNoFilter('irx-staff');
+        }
+
+        return $member;
     }
 
     public static function get_login_form(Controller $controller)
